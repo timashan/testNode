f6448b70fb7627bc3d3dba3d905e667bcce235ff
timashan <kavishkatimashan@gmail.com>
2022-08-05 00:33:28 +0530

    Added frontend

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const Path = require("path");
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.json({
-    status: "success",
-  });
+  res.sendFile(Path.resolve(__dirname, "public", "index.html"));
 });
+
+app.use(express.static(Path.join("public")));
 
 const PORT = 5000;
 app.listen(PORT, () => {
